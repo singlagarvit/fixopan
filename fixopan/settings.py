7bd6bb2b926d2ec93e42be4ba040e261e0f49331
Garvit Singla <singlagarvit68@gmail.com>
2019-11-11 15:24:52 +0530

    secret_key update

--- fixopan/settings.py
+++ fixopan/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4c1u2#u^s!fd8ow=4%&+g9y@^9fpy#ts1wi6m&ui+s5xp5bx_)'
+# SECRET_KEY = '4c1u2#u^s!fd8ow=4%&+g9y@^9fpy#ts1wi6m&ui+s5xp5bx_)'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cryptic-journey-92310.herokuapp.com']
 
 
 # Application definition
