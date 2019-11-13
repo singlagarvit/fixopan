59428e27ac63757bd0a33bf87ac257c9ae92c0c3
Garvit Singla <singlagarvit68@gmail.com>
2019-11-14 00:52:58 +0530

    Complete webpages

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import home_view
+from .views import home_view, about_view, advantages_view, waterstop_view, profiles_view, contruction_view
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -9,4 +9,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home_view, name="index"),
+    path('about-us/', about_view, name="about"),
+    path('advantages/', advantages_view, name="advnatage"),
+    path('pvc-waterstop/', waterstop_view, name="waterstop"),
+    path('pvc-profiles/', profiles_view, name="profiles"),
+	path('construction-joints/', contruction_view, name="construction"),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- home/views.py
+++ home/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render, redirect
 
 def home_view(request):
 	return render(request, 'index.html', {})
+
+def about_view(request):
+	return render(request, 'about.html', {})
+
+def advantages_view(request):
+	return render(request, 'advantages.html', {})
+
+def waterstop_view(request):
+	return render(request, 'waterstop.html', {})
+
+def profiles_view(request):
+	return render(request, 'profiles.html', {})
+
+def contruction_view(request):
+	return render(request, 'construction-joints.html', {})
