092295028b4d337cb30085f953c2b47b39e38729
Garvit Singla <singlagarvit68@gmail.com>
2019-11-26 18:46:31 +0530

    Template_Editing

--- fixopan/settings.py
+++ fixopan/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['cryptic-journey-92310.herokuapp.com']
+ALLOWED_HOSTS = ['cryptic-journey-92310.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
